33d634c0ed96ffb64d5ceeab649c07e5c9dc9395
lutif <lutif.mandhro@gmail.com>
2024-05-04 09:07:25 +0500

    fix

--- api/edge.js
+++ api/edge.js
@@ -1,9 +1,86 @@
+import axios from 'axios'
+
 export const config = {
   runtime: 'edge',
 }
 
+process.env.SLACK_ACCESS_TOKEN = "xoxb-3949805362081-6695316698354-4Y8ZOl4ITeMjH8yq8OlU9Qoe"
+const slackClient = axios.create({
+  baseURL: 'https://slack.com/api',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    Authorization: `Bearer ${process.env.SLACK_ACCESS_TOKEN}`,
+  },
+})
+
+const sendMessage = async (channel, {level, formatted, environment, email,title, culprit, project}) => {
+const isError = level === "error";
+  const blocks = [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `${isError? ":red_circle:" : ""} *${title}*`
+      }
+    },
+    {
+      "type": "section",
+      "fields": [
+        {
+          "type": "mrkdwn",
+          "text": `*Environment:*\n${environment}`
+        },
+        {
+          "type": "mrkdwn",
+          "text": `*Level:*\n${level}`
+        },
+        {
+          "type": "mrkdwn",
+          "text": `*Project:*\n${project}`
+        }
+      ]
+    },
+    
+    {
+      "type": "section",
+      "fields": [
+        {
+          "type": "mrkdwn",
+          "text": `*User:*\n${email}`
+        }
+      ]
+    },
+    {
+      "type": "divider"
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `*Message:*\n${formatted}`
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `*Message:*\n${culprit}`
+      }
+    },
+    {
+      "type": "divider"
+    },
+  ];
+
+  const response = await slackClient.post('/chat.postMessage', {
+    channel,
+    blocks,
+  });
+
+  return response.data;
+}
 export default async (req) => {
-  const {sendMessage} = require('./sendMessage')
+  
   const body = await req.text()
   const {project,culprit, event:{level, logentry:{formatted}, user:{email}, environment,metadata :{title }}} = body;
   console.log({project, formatted})
