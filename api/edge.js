546359db88f8eec512e425e25af478cc320068e2
lutif <lutif.mandhro@gmail.com>
2024-05-04 07:51:57 +0500

    return request body

--- api/edge.js
+++ api/edge.js
@@ -2,4 +2,11 @@ export const config = {
   runtime: 'edge',
 }
 
-export default (req) => new Response('Hello world!')
+export default (req) => {
+  console.log(req.body)
+  return {
+    status: 200,
+    body: req.body,
+
+  }
+}
